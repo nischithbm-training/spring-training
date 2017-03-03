dcf634bd0b1e5dc47a2056d38125e066669ae024
nbm <Nischith_BM@intuit.com>
2017-03-03 15:00:02 +0530

    Spring data rest Sample using SpringBoot

---
+++ spring-data-sample/src/main/java/org/nischithbm/sample/springdata/Person.java
@@ -0,0 +1,36 @@
+package org.nischithbm.sample.springdata;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Person implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+
+	private String firstName;
+	private String lastName;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+}
